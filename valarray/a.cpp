8ab9724cc4fb859c63c1080542a11c0a289559ad
iamslash <iamslash@gmail.com>
2016-10-18 18:35:00 +0900

    added valarray

---
+++ valarray/a.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2016 by iamslash
+
+// valarray constructor example
+#include <iostream>     // std::cout
+#include <valarray>     // std::valarray, std::slice
+
+int main ()
+{
+  int init[]= {10,20,30,40};
+  std::valarray<int> first;                             // (empty)
+  std::valarray<int> second (5);                        // 0 0 0 0 0
+  std::valarray<int> third (10,3);                      // 10 10 10
+  std::valarray<int> fourth (init,4);                   // 10 20 30 40
+  std::valarray<int> fifth (fourth);                    // 10 20 30 40
+  std::valarray<int> sixth (fifth[std::slice(1,2,1)]);  // 20 30
+
+  std::cout << "sixth sums " << sixth.sum() << '\n';
+
+  return 0;
+}
