45eb2dbf3691a0e9ab3d96d405fc960d587a6e64
David Sun <iamslash@gmail.com>
2017-10-24 22:37:58 +0900

    update priority queue

--- library/priority_queue/a.cpp
+++ library/priority_queue/a.cpp
@@ -1,48 +1,51 @@
 // Copyright (C) 2016 by iamslash
 
 // constructing priority queues
-#include <iostream>       // std::cout
 #include <queue>          // std::priority_queue
 #include <vector>         // std::vector
 #include <functional>     // std::greater
 
-class mycomparison {
-  bool reverse;
+void test_1() {
+  std::vector<int> v = {2, 1, 4, 6, 4, 9, 0};
+  std::priority_queue<int> pq(v.begin(), v.end());
+  printf("%d\n", pq.top());
+}
+
+void test_2() {
+  auto mycomp = [](const int& e1, const int& e2) {
+    return e1 > e2;
+  };
+  std::vector<int> v = {2, 1, 4, 6, 4, 9, 0};
+  std::priority_queue<int, std::vector<int>, decltype(mycomp)>
+      pq(mycomp, v);
+  printf("%d\n", pq.top());
+}
+
+class MyComp {
+ private:
+  bool m_b_reverse;
  public:
-  explicit mycomparison(const bool& revparam = false) {
-    reverse = revparam;
+  explicit MyComp(const bool& b_reverse = false) {
+    m_b_reverse = b_reverse;
   }
-  bool operator() (const int& lhs, const int&rhs) const {
-    if (reverse)
-      return (lhs > rhs);
-    else
-      return (lhs < rhs);
+  bool operator() (const int& lhs, const int& rhs) {
+    if (m_b_reverse)
+      return lhs > rhs;
+    return lhs < rhs;
   }
 };
 
+void test_3() {
+  std::vector<int> v = {2, 1, 4, 6, 4, 9, 0};
+  std::priority_queue<int, std::vector<int>, MyComp>
+      pq(MyComp(false), v);
+  printf("%d\n", pq.top());
+}
+
 int main() {
-  // ::constructor
-  int myints[] = {10, 60, 50, 20};
-
-  // std::priority_queue<int> first;
-  // std::priority_queue<int> second(myints, myints + 4);
-  // std::priority_queue<int, std::vector<int>,
-  //                     std::greater<int> > third(myints, myints + 4);
-  // // using mycomparison:
-  // typedef std::priority_queue<int, std::vector<int>, mycomparison> mypq_type;
-
-  // mypq_type fourth;                       // less-than comparison
-  // mypq_type fifth(mycomparison(true));   // greater-than comparison
-
-  //
-  // std::priority_queue<int, std::vector<int>, std::less<int> >
-  //     max_heap(myints, myints + 4);
-  // std::priority_queue<int, std::vector<int>, std::greater<int> >
-  //     min_heap(myints, myints + 4);
-  // printf("%d %d\n", max_heap.top(), min_heap.top());
-
-  std::priority_queue<int, std::vector<int>, std::less<int> > q;
-  printf("%d\n", q.top());
-
+  test_1();
+  test_2();
+  test_3();
+  
   return 0;
 }
